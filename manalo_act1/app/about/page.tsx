d2032a2b5b21d623668ea6dad63df477d20509f2
IwissManalo <altheairishmanalo@gmail.com>
2025-01-12 14:52:57 +0800

    Create page.tsx

---
+++ manalo_act1/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+      <div className="p-6">
+        <h1 className="text-4xl font-bold">About Me</h1>
+        <p className="mt-4 text-lg">Hello, I'm Althea Irish Manalo from BSIT 3A and this is my first App Dev activity using next js. I hope to learn more and absorb a lot of knowledge under Mr. Dave Mercado's subject bc I kinda struggle when it comes to backend and framework developments😅</p>
+      </div>
+    );
+  }
+  
