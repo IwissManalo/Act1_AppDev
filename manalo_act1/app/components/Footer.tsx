f930ca224512b319fa9592bf58a07023b2caf689
IwissManalo <altheairishmanalo@gmail.com>
2025-01-12 14:52:59 +0800

    Create Footer.tsx

---
+++ manalo_act1/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-pink-500 text-white p-4 text-right fixed bottom-0 left-0 w-full">
+      <p>&copy; Althea Irish Manalo 2025</p>
+    </footer>
+  )
+}
